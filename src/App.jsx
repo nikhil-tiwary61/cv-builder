735f126b8262f967ba79276f8847d00bc3ab923c
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-20 16:39:46 +0530

    add css to GeneralInformation component
    add minor adjustments to GeneralInformation with placeholders and positioning

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import GeneralInformation from './components/GeneralInformation'
 
 export default function App() {
   return (
-    <>
+    <div className="main-page">
     <div className="sidebar">
       <GeneralInformation />
     </div>
-    <div className="resume"></div>
-    </>
+    <div className="resume">
+      Resume
+    </div>
+    </div>
   )
 }
--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -1,16 +1,25 @@
+import "../styles/GeneralInformation.css"
+
 const GeneralInformation = () => {
   return (
-    <div>
-      <form action="">
+    <div className="general-information">
+      <form action="" className="form-general-information">
+        <legend>General Information</legend>
+        <br />
         <label htmlFor="">Full Name
-        <input type="text" />
+        <br />
+        <input type="text" placeholder="Enter Your name"/>
         </label>
         <label htmlFor="">Email
-        <input type="text" />
+        <br />
+        <input type="email" placeholder="Enter email"/>
         </label>
         <label htmlFor="">Phone Number
-        <input type="text" />
+        <br />
+        <input type="number" placeholder="Enter phone number"/>
         </label>
+        <br />
+        <button>Submit</button>
       </form>
     </div>
   )
