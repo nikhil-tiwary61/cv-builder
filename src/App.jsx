3643b926e430fc42f842c8a398b6728d40a18664
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-20 17:36:07 +0530

    feat: add states for GeneralInformation component
    render information in real time on resume

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import './App.css'
+import { useState } from "react"
 import GeneralInformation from './components/GeneralInformation'
 
 export default function App() {
+  //state variables for General Information
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  //handle functions to change state variables of General Information
+  function changeName(e){
+    setName(e.target.value);
+  }
+  function changeEmail(e){
+    setEmail(e.target.value);
+  }
+  function changePhoneNumber(e){
+    setPhoneNumber(e.target.value);
+  }
+
   return (
     <div className="main-page">
     <div className="sidebar">
-      <GeneralInformation />
+      <GeneralInformation changeName={changeName} changeEmail={changeEmail} changePhoneNumber={changePhoneNumber} />
     </div>
     <div className="resume">
       Resume
+      {name}{email}{phoneNumber}
     </div>
     </div>
   )
--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -1,6 +1,6 @@
 import "../styles/GeneralInformation.css"
 
-const GeneralInformation = () => {
+function GeneralInformation({ changeName, changeEmail, changePhoneNumber }){
   return (
     <div className="general-information">
       <form action="" className="form-general-information">
@@ -8,21 +8,21 @@ const GeneralInformation = () => {
         <br />
         <label htmlFor="">Full Name
         <br />
-        <input type="text" placeholder="Enter Your name"/>
+        <input type="text" placeholder="Enter Your name" onChange={changeName} />
         </label>
         <label htmlFor="">Email
         <br />
-        <input type="email" placeholder="Enter email"/>
+        <input type="email" placeholder="Enter email" onChange={changeEmail} />
         </label>
         <label htmlFor="">Phone Number
         <br />
-        <input type="number" placeholder="Enter phone number"/>
+        <input type="text" placeholder="Enter phone number" onChange={changePhoneNumber} />
         </label>
-        <br />
-        <button>Submit</button>
+        {/* <br /> */}
+        {/* <input type="submit" /> */}
       </form>
     </div>
   )
 }
 
-export default GeneralInformation
+export default GeneralInformation;
