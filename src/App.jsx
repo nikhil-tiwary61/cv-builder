92bb11c98a858ad4202d777471a917b4ba5f66c2
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-20 16:11:58 +0530

    feat: add component GeneralInformation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import GeneralInformation from './components/GeneralInformation'
 
 export default function App() {
   return (
     <>
+    <div className="sidebar">
+      <GeneralInformation />
+    </div>
+    <div className="resume"></div>
     </>
   )
 }
---
+++ src/components/GeneralInformation.jsx
@@ -0,0 +1,19 @@
+const GeneralInformation = () => {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">Full Name
+        <input type="text" />
+        </label>
+        <label htmlFor="">Email
+        <input type="text" />
+        </label>
+        <label htmlFor="">Phone Number
+        <input type="text" />
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default GeneralInformation
