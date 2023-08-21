9b53e2dbf127a43979c9f7600843ddad57d00c54
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-21 18:25:11 +0530

    add style to Educarion.css

--- src/components/resume/Education.jsx
+++ src/components/resume/Education.jsx
@@ -1,12 +1,15 @@
+import "../../styles/Education.css";
+
 export default function Education({
   institute,
   fieldOfStudy,
   yearOfGraduation,
 }) {
   return (
-    <section>
+    <section className="resume-education">
       <h2>Education</h2>
-      <p>Field of study : {institute}</p>
+      <hr className="horizontal-line" />
+      <p>Name of the Institution : {institute}</p>
       <p>Field of study : {fieldOfStudy}</p>
       <p>Year of graduation : {yearOfGraduation}</p>
     </section>
