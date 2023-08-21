c3f9ece4fc9856fac52f0bcad5c4015b37f03e12
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-21 18:07:24 +0530

    add style to GeneralInformation.css to render on resume

--- src/components/resume/GeneralInformation.jsx
+++ src/components/resume/GeneralInformation.jsx
@@ -1,9 +1,15 @@
-export default function GeneralInformation({name, email, phoneNumber}){
-    return(
-        <section>
-        <h1>{name}</h1>
+import "../../styles/GeneralInformation.css";
+
+export default function GeneralInformation({ name, email, phoneNumber }) {
+  return (
+    <section className="resume-general-information">
+      <div className="username-box">
+        <h1 className="username">{name}</h1>
+      </div>
+      <div className="information">
         <p>Email : {email}</p>
-        <p>Pnone Number : {phoneNumber}</p>
-        </section>
-    );
+        <p>Phone No : {phoneNumber}</p>
+      </div>
+    </section>
+  );
 }
