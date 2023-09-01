63a85695483efc4098b0985a406a6f2f604add32
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-01 19:09:39 +0530

    feat: print the resume or save as a pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Logo from "./components/sidebar/Logo";
 import GeneralInformationForm from "./components/sidebar/GeneralInformationForm";
 import GeneralInformation from "./components/resume/GeneralInformation";
@@ -8,9 +8,11 @@ import Education from "./components/resume/Education";
 import ExperienceForm from "./components/sidebar/ExperienceForm";
 import Experience from "./components/resume/Experience";
 import { dummyResume } from "./data/dummyResume";
+import ReactToPrint from "react-to-print";
 
 export default function App() {
   const [resume, setResume] = useState({ ...dummyResume });
+  let componentRef = useRef();
 
   //handle functions to change resume
   function changeResume(e) {
@@ -27,9 +29,13 @@ export default function App() {
         <GeneralInformationForm resume={resume} changeResume={changeResume} />
         <EducationForm resume={resume} changeResume={changeResume} />
         <ExperienceForm resume={resume} changeResume={changeResume} />
+        <ReactToPrint
+          trigger={() => <button>Print</button>}
+          content={() => componentRef}
+        />
       </div>
       <div className="right-side-view">
-        <div className="resume">
+        <div className="resume" ref={(el) => (componentRef = el)}>
           <GeneralInformation resume={resume} />
           <Education resume={resume} />
           <Experience resume={resume} />
